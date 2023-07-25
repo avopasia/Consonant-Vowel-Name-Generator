e4181f2f4c791edf8c229254f6043b45b078d288
Avopasia <62235855+avopasia@users.noreply.github.com>
2023-07-25 11:26:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ const consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", 
 const vowels = ["a", "e", "i", "o", "u"];
 const chars = [];
 
-const output = document.querySelector("#output");
-
 const submit = document.querySelector("#submit").addEventListener("click", (chars) => {
     let wordArray = [];
     const input = document.querySelector("#input").value;
     chars = input.split("");
     const grid = document.querySelector("#grid-container");
     grid.textContent = "";
-
     for (let i = 0; i < 40; i++) {
+        wordArray.push("a");
+        wordArray.push("d");
         chars.forEach(char => {
             if (char == "c" || char == "C") {
                 wordArray.push(consonants[Math.floor(Math.random() * consonants.length)]);
